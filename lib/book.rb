0aa7c7896c3e743ecc91a5e7d41557fb4c97850d
kitte anderson <kitteanderson@gmail.com>
2019-02-11 20:20:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title :genre
+  attr_reader :title, :genre
 
   def initialize(title)
     @title = title
@@ -16,5 +16,4 @@ class Book
     @genre = genre
     GENRES << genre
   end
-
 end
